eeee24134c5c2bc9e5fcd47df223db1f4ad70647
Yuri Galvao <yuri.gvao@gmail.com>
2024-01-27 18:17:46 -0300

    fix userRepository interface

--- src/application/useCases/SignUpUser.ts
+++ src/application/useCases/SignUpUser.ts
@@ -2,7 +2,7 @@ import User from "../../domain/entity/User"
 import { AddRepository } from "../repository/Repository.interface"
 
 export default class SignUpUser {
-    constructor(readonly userRepository: AddRepository) { }
+    constructor(readonly userRepository: AddRepository<User>) { }
 
     async execute({ email, password }: Input): Promise<Output> {
         const user = User.create({ email, password })
