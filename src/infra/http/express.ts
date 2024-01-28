358ba15a07bfc4b5db897fbdfd5b0e76ec0121c2
Yuri Galvao <yuri.gvao@gmail.com>
2024-01-28 15:40:50 -0300

    files statics with express

--- src/infra/http/express.ts
+++ src/infra/http/express.ts
@@ -2,12 +2,14 @@ import Express from 'express'
 import { createServer } from 'http'
 
 import authRoute from '../routes/auth'
+import path from 'path'
 
 const app = Express()
 const PORT = process.env.PORT || 3000
 
 app.use(Express.json())
 app.use('/api', authRoute)
+app.use(Express.static(path.join(__dirname, '../../../public')))
 
 const server = createServer(app)
 
