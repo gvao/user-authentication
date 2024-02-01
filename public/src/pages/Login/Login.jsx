a5dcc69596ab16623d4c826651da7b48a84ef69e
antoniomarcuss <105389155+antoniomarcuss@users.noreply.github.com>
2024-02-01 15:54:42 -0300

    Criando Hook dinâmico para realizar o Fetch das páginas de login e cadastro!

--- public/src/pages/Login/Login.jsx
+++ public/src/pages/Login/Login.jsx
@@ -1,22 +1,58 @@
 import style from './Login.module.css'
+import { useState  } from 'react'
 import { Link } from 'react-router-dom'
+import { useSend } from '../../hooks/useSend'
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const {sendRequest, error:sendError} = useSend()
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    const user = {
+      email,
+      password
+    }
+
+    try {
+      await sendRequest(user, 'http://localhost:3000/api/login')
+      console.log(user);
+    } catch (error) {
+      console.log(sendError);
+    }
+
+
+  }
   return (
     <div className={style.login}>
-        <h1>Entrar</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
+            <h1>Entrar</h1>
             <label>
-            <span>E-mail</span>
-            <input type="email" placeholder='Digite seu E-mail' />
-            </label>
-            <label>
-            <span>Senha</span>
-            <input type="password" placeholder='Digite sua senha' />
-            </label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            placeholder='Digite seu E-mail'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} 
+            required/>
+        </label>
+
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            placeholder='Digite sua senha'
+            name='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+            required/>
+        </label>
             <button className='btn'>Entrar</button>
+           
         </form>
-        <p></p> 
-       <Link to='/register'><button>Criar Cadastro</button></Link>
+       <Link to='/register'><button className='btn_resgiter'>Criar Cadastro</button></Link>
     </div>
   )
 }
