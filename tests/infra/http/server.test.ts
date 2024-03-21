8f85c3a5627ee3b4d2b84ccac4e64363853b22e3
Yuri Galvao <yuri.gvao@gmail.com>
2024-03-21 14:31:29 -0300

    update server

--- tests/infra/http/server.test.ts
+++ tests/infra/http/server.test.ts
@@ -3,19 +3,21 @@ import http from 'http'
 
 import Server from "../../../src/infra/http/Server"
 import ExpressAdapter from "../../../src/infra/adapter/ExpressAdapter"
-import fetcher from "../../utils/fetcher"
+import Fetcher from "../../utils/fetcher"
 
 const _PORT = 3333
 
 describe('Server', () => {
     let _server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>
+    const url = Fetcher.host + _PORT
+    const fetcher = new Fetcher(url)
 
     beforeAll(async () => {
         const expressAdapter = new ExpressAdapter()
         const server = new Server(expressAdapter)
         _server = server.listen(_PORT)
 
-        server.addRoute('/', function () {
+        server.addRoute({ path: '/' }, function () {
             return {
                 status: 200,
                 data: {
@@ -28,7 +30,7 @@ describe('Server', () => {
     })
 
     it('should ', async () => {
-        const response = await fetcher('/')
+        const response = await fetcher.execute('/')
         expect(response.ok).toBeTruthy()
         expect(response.status).toBe(200)
         const json = await response.json()
