6720f273d189c188c4af26b2bd71787a9aca626b
Yuri Galvao <yuri.gvao@gmail.com>
2024-03-21 14:28:48 -0300

    updated Fetcher

--- tests/utils/fetcher.ts
+++ tests/utils/fetcher.ts
@@ -1,34 +1,17 @@
-// export default async function fetcher(
-//     path: string,
-//     method: "GET" | "POST" = "GET",
-//     options: FetcherOptions = { port: 3000 }
-// ) {
-//     const url = `http://localhost:${options.port}${path}`
-//     const response = await fetch(url, {
-//         method,
-//         headers: {
-//             contentType: "application/json",
-//         },
-//         body: JSON.stringify(options.body)
-//     })
-//     if (!response.ok) throw new Error('bad request')
-//     return response
-// }
-
-
 export default class Fetcher {
     constructor(readonly url: string) { }
     static host: string = "http://localhost:"
-    
+
     async execute(path: string, options: Options = { method: "GET" }) {
         const uri = this.url + path
-        const response = await fetch(uri, {
+        const header = {
             method: options.method,
             headers: {
-                contentType: "application/json",
+                "Content-Type": "Application/json",
             },
             body: JSON.stringify(options.body)
-        })
+        }
+        const response = await fetch(uri, header)
         if (!response.ok) throw new Error('bad request')
         return response
     }
