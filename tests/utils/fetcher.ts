39d4f9a30615360ba8e14f5b4f6d34e6c4a84182
Yuri Galvao <yuri.gvao@gmail.com>
2024-03-21 13:42:36 -0300

    create Fetcher

--- tests/utils/fetcher.ts
+++ tests/utils/fetcher.ts
@@ -1,7 +1,40 @@
-export default async function fetcher(path: string, port = 3000) {
-    const url = `http://localhost:${port}${path}`
-    console.log(url)
-    const response = await fetch(url)
-    if (!response.ok) throw new Error('bad request')
-    return response
+// export default async function fetcher(
+//     path: string,
+//     method: "GET" | "POST" = "GET",
+//     options: FetcherOptions = { port: 3000 }
+// ) {
+//     const url = `http://localhost:${options.port}${path}`
+//     const response = await fetch(url, {
+//         method,
+//         headers: {
+//             contentType: "application/json",
+//         },
+//         body: JSON.stringify(options.body)
+//     })
+//     if (!response.ok) throw new Error('bad request')
+//     return response
+// }
+
+
+export default class Fetcher {
+    constructor(readonly url: string) { }
+    static host: string = "http://localhost:"
+    
+    async execute(path: string, options: Options = { method: "GET" }) {
+        const uri = this.url + path
+        const response = await fetch(uri, {
+            method: options.method,
+            headers: {
+                contentType: "application/json",
+            },
+            body: JSON.stringify(options.body)
+        })
+        if (!response.ok) throw new Error('bad request')
+        return response
+    }
+}
+
+type Options = {
+    method?: "GET" | "POST",
+    body?: any,
 }
