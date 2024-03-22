41440b31429c0674bbff9996ef1528da072399c3
Yuri Galvao <yuri.gvao@gmail.com>
2024-03-22 11:48:24 -0300

    insert new route in server

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
+import LoginUser from "./application/useCases/LoginUser";
+import SignUpUser from "./application/useCases/SignUpUser";
+import Route from "./domain/Route";
 import ExpressAdapter from "./infra/adapter/ExpressAdapter";
 import Server from "./infra/http/Server";
+import UserRepositoryInMemory from "./infra/repositories/UserRepositoryInMemory";
 
 const PORT = Number(process.env.PORT) || 3000
 
 const expressAdapter = new ExpressAdapter()
 const serverHttp = new Server(expressAdapter)
 
-serverHttp.addRoute('/test', () => {
-    return {
-        status: 200,
-        data: {
-            message: `test`
-        }
-    }
+const userRepository = new UserRepositoryInMemory()
+
+const signupRoute = new Route('/user/signup', 'POST', async ({ body: { email, password, name } }) => {
+    const signup = new SignUpUser(userRepository)
+    const data = await signup.execute({ email, name, password })
+    return { status: 200, data }
 })
+
+const loginUser = new Route('/user/login', 'GET', async ({ body: { email, password } }) => {
+    const loginUser = new LoginUser(userRepository)
+    const data = await loginUser.execute({ email, password })
+    return { status: 200, data }
+})
+
+serverHttp.addRoute(signupRoute)
+serverHttp.addRoute(loginUser)
 
 export const server = serverHttp.listen(PORT)
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,15 +1,33 @@
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, describe, expect, it } from 'vitest'
 
 import { server } from '../src'
-import fetcher from './utils/fetcher'
+import Fetcher from './utils/fetcher'
 
 describe('e2e', () => {
+    const url = Fetcher.host + '3000'
+    const fetcher = new Fetcher(url)
 
-    it('should', async () => {
-        const response = await fetcher('/test')
+    let fakeToken: string
+
+    it('/user/signup', async () => {
+        const input = { email: "any_email@gmail.com", password: "any_password", name: 'any_name' }
+        const response = await fetcher.execute('/user/signup', { method: 'POST' })
+        expect(response.status).toEqual(200)
         const json = await response.json()
-        expect(json.message).toBe('test')
+        expect(json.token).toBeDefined()
+        fakeToken = json.token
+    })
+    it('/user/login', async () => {
+        const response = await fetcher.execute('/user/login', { method: 'GET', headers: { authentication: `Bearer ${fakeToken}` } })
+        expect(response.ok).toBeTruthy();
+        const result = await response.json()
+        console.log(result)
+        // expect(result.token).toBe
     })
 
-    afterAll(() => { server.close(() => { console.log('server closed!') }) })
+    afterAll(() => {
+        server.close(() => {
+            // console.log('server closed!')
+        })
+    })
 })
